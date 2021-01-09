d41b7df379049f2f6ee80aba3263d41cbe3f2d52
Roy Are Øvrebotten <69842808+raovrebo@users.noreply.github.com>
2021-01-09 20:24:32 +0100

    Updated index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// question 1
+
+var numValue = 46;
+var boolValue = true;
+var strValue = "This is Module Assignment 1";
+
+// question 2
+
+var fname ="Roy";
+var lname ="Øvrebotten";
+var fullName = (fname + " " + lname);
+console.log(fullName);
+
+// question 3
+
+var x = "frog";
+console.log("The type of frog is " + typeof x);
+
+// question 4
+
+var orderHasShipped = true;
+if (orderHasShipped === true) {
+    console.log("The order shipped");
+} 
+else {
+    console.log("The order did not ship");
+}
+
+// question 5
+
+for (var i = 7; i <= 13; i++) {
+    console.log(i);
+}
